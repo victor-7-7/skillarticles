fd5c58e4eff68b2655498feb6dc3fdb5520ffd1d
victor-7-7 <Victor.7.7@ya.ru>
2020-05-05 20:39:22 +0700

    hometask_3. Assignment 4 done.

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/BottombarBehavior.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/BottombarBehavior.kt
@@ -1,4 +1,42 @@
 package ru.skillbranch.skillarticles.ui.custom.behaviors
 
-class BottombarBehavior {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.widget.NestedScrollView
+import com.google.android.material.behavior.HideBottomViewOnScrollBehavior
+import ru.skillbranch.skillarticles.ui.custom.Bottombar
+
+
+class BottombarBehavior @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : HideBottomViewOnScrollBehavior<Bottombar>() {
+
+    override fun layoutDependsOn(
+        parent: CoordinatorLayout,
+        child: Bottombar,
+        dependency: View
+    ): Boolean {
+        /** мы хотим реагировать только на изменения разметки
+         * виджета NestedScrollView */
+        return dependency is NestedScrollView
+    }
+
+    /** Метод вызывается всякий раз, когда с виджетом, который находится
+     * в CoordinatorLayout и изменения которого мы отслеживаем (NestedScrollView),
+     * что-то происходит */
+    override fun onDependentViewChanged(
+        parent: CoordinatorLayout,
+        child: Bottombar,
+        dependency: View
+    ): Boolean {
+//        val translationY = 0f.coerceAtMost(
+//            dependency.translationY - dependency.height)
+//        child.translationY = translationY
+//        return true
+        return super.onDependentViewChanged(parent, child, dependency)
+    }
 }
